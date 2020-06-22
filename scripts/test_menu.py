ba02cffa46bc192b0e50ac715c94b08509188575
kissiy179 <kissiy179@gmail.com>
2020-06-22 14:22:23 +0900

    テスト用メニューを動作するものに変更

--- scripts/test_menu.py
+++ scripts/test_menu.py
@@ -1,24 +1,20 @@
 import maya.cmds as cmds
 import reloadable_menu; reload(reloadable_menu)
 
-@reloadable_menu.reloadable_menu('test_menu')
+@reloadable_menu.reloadable_menu('test_menu1')
 def test_menu():
     print 'test1'
-    cmds.menuItem(label='Character',tearOff=True,subMenu=True)
-    cmds.menuItem(label='blendShapeI/O', command='import pegasus_blendshapeio; pegasus_blendshapeio.main()')
-    cmds.menuItem(label='blendShapeI/O', command='import pegasus_blendshapeio; pegasus_blendshapeio.main()')
-    cmds.menuItem(label='test', command='import pegasus_blendshapeio; pegasus_blendshapeio.main()')
-    cmds.menuItem(label='b', command='import pegasus_blendshapeio; pegasus_blendshapeio.main()')
-    cmds.setParent('..', menu=True)
+    # cmds.menuItem(label='Character',tearOff=True,subMenu=True)
+    cmds.menuItem(label='test1_1', command='print("exec_test1-1")')
+    cmds.menuItem(label='test1_2', command='print("exec_test1-2")')
+    # cmds.setParent('..', menu=True)
 
 @reloadable_menu.reloadable_menu('test_menu2')
 def test_menu2():
     print 'test2'
-    cmds.menuItem(label='Character',tearOff=True,subMenu=True)
-    cmds.menuItem(label='blendShapeI/O', command='import pegasus_blendshapeio; pegasus_blendshapeio.main()')
-    cmds.menuItem(label='blendShapeI/O', command='import pegasus_blendshapeio; pegasus_blendshapeio.main()')
-    cmds.menuItem(label='test', command='import pegasus_blendshapeio; pegasus_blendshapeio.main()')
-    cmds.menuItem(label='b', command='import pegasus_blendshapeio; pegasus_blendshapeio.main()')
+    cmds.menuItem(label='parent_menu',tearOff=True,subMenu=True)
+    cmds.menuItem(label='test2_1', command='print("exec_test2-1")')
+    cmds.menuItem(label='test2_2', command='print("exec_test2-2")')
     cmds.setParent('..', menu=True)
 
 def show():
