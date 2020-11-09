d239feaefc797607678957e41e9121ead374cd75
kissiy179 <kissiy179@gmail.com>
2020-11-09 21:16:33 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ setup(
     version='0.1.0',
     author='kissiy179@gmail.com',
     url='https://github.com/kissiy179/maya_reloadable_menu',
-    py_modules=['python/reloadable_menu'],
+    package_dir={"": "python"},
+    py_modules=['reloadable_menu'],
     include_package_data=True,
     python_requires=">=2.7",
     install_requires=[
