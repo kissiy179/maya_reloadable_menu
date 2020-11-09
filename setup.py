917cf394e7010f4d0864c721f1d3f3ea60b077d5
kissiy179 <kissiy179@gmail.com>
2020-11-09 21:07:01 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='maya_reloadable_menu',
+    version='0.1.0',
+    author='kissiy179@gmail.com',
+    url='https://github.com/kissiy179/maya_reloadable_menu',
+    py_modules=['reloadable_menu'],
+    include_package_data=True,
+    python_requires=">=2.7",
+    install_requires=[
+    ]
+)
