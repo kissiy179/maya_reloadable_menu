da83e72ee8fb810a40f72d7c0af7f18b7a94b5e8
kissiy179 <kissiy179@gmail.com>
2021-03-04 02:13:56 +0900

    test_commandsメニューにReloadアイテムを追加

--- scripts/userSetup.py
+++ scripts/userSetup.py
@@ -1,8 +1,26 @@
-import sys
-from maya import utils
-import test_menu
+#encoding: utf-8
+import maya.utils
+import maya.cmds as cmds
+import reloadable_menu
 
-#utils.executeDeferred(test_menu.show)
+def show_message(message):
+    print(message)
+    cmds.inViewMessage(assistMessage=message,
+                        position='midCenter',
+                        fade=True,
+                        # fadeStayTime=3000,
+                        )
 
 
+# メニュー定義
+@reloadable_menu.reloadable_menu('test commands')
+def create_menu_():
+    # cmds.setParent('MayaWindow')
+    # cmds.menu(label='test commands', tearOff=True)
+    cmds.menuItem(l='test command 1', command='show_message("test command 1")')
+    cmds.menuItem(l='test command 2', command='show_message("test command 2")')
 
+# メニュー作成
+maya.utils.executeDeferred(create_menu_)
+
+print create_menu_.__module__, '---'
