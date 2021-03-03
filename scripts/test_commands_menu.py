70d59b78e273c570dc56ec5c12423f7067e6c1a7
kissiy179 <kissiy179@gmail.com>
2021-03-04 02:35:03 +0900

    Test Commandsメニュー作成コードをuserSetup.pyからtest_commands_menu.pyに分離

--- scripts/test_commands_menu.py
+++ scripts/test_commands_menu.py
@@ -1,22 +1,21 @@
+#encoding: utf-8
+
 import maya.cmds as cmds
-import reloadable_menu; reload(reloadable_menu)
+import maya.cmds as cmds
+import reloadable_menu
 
-@reloadable_menu.reloadable_menu('test_menu1')
-def test_menu():
-    print 'test1'
-    # cmds.menuItem(label='Character',tearOff=True,subMenu=True)
-    cmds.menuItem(label='test1_1', command='print("exec_test1-1")')
-    cmds.menuItem(label='test1_2', command='print("exec_test1-2")')
-    # cmds.setParent('..', menu=True)
+def show_message(message):
+    print(message)
+    cmds.inViewMessage(assistMessage=message,
+                        position='midCenter',
+                        fade=True,
+                        # fadeStayTime=3000,
+                        )
 
-@reloadable_menu.reloadable_menu('test_menu2')
-def test_menu2():
-    print 'test2'
-    cmds.menuItem(label='parent_menu',tearOff=True,subMenu=True)
-    cmds.menuItem(label='test2_1', command='print("exec_test2-1")')
-    cmds.menuItem(label='test2_2', command='print("exec_test2-2")')
-    cmds.setParent('..', menu=True)
+@reloadable_menu.reloadable_menu('Test Commands')
+def create_menu_():
+    # cmds.setParent('MayaWindow')
+    # cmds.menu(label='Test Commands', tearOff=True)
+    cmds.menuItem(l='Test Command 1', command='import test_commands_menu; test_commands_menu.show_message("Test Command 1")')
+    cmds.menuItem(l='Test Command 2', command='import test_commands_menu; test_commands_menu.show_message("Test Command 2")')
 
-def show():
-    test_menu()
-    test_menu2()
--- scripts/userSetup.py
+++ scripts/userSetup.py
@@ -1,26 +1,7 @@
 #encoding: utf-8
 import maya.utils
 import maya.cmds as cmds
-import reloadable_menu
-
-def show_message(message):
-    print(message)
-    cmds.inViewMessage(assistMessage=message,
-                        position='midCenter',
-                        fade=True,
-                        # fadeStayTime=3000,
-                        )
-
-
-# メニュー定義
-@reloadable_menu.reloadable_menu('test commands')
-def create_menu_():
-    # cmds.setParent('MayaWindow')
-    # cmds.menu(label='test commands', tearOff=True)
-    cmds.menuItem(l='test command 1', command='show_message("test command 1")')
-    cmds.menuItem(l='test command 2', command='show_message("test command 2")')
+import test_commands_menu
 
 # メニュー作成
-maya.utils.executeDeferred(create_menu_)
-
-print create_menu_.__module__, '---'
+maya.utils.executeDeferred(test_commands_menu.create_menu_)
