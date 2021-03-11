6e63ceec36c5ea1f0991f1e0a1f6e21223998162
kissiy179 <kissiy179@gmail.com>
2021-03-11 18:47:40 +0900

    setup.py、requirements.txtを再追加

---
+++ setup.py
@@ -0,0 +1,24 @@
+from glob import glob
+from os.path import basename
+from os.path import splitext
+from os.path import exists
+from setuptools import setup
+from setuptools import find_packages
+
+def _requires_from_file(filename):
+    if not exists(filename):
+        return []
+    
+    return open(filename).read().splitlines()
+
+setup(
+    name='maya_reloadable_menu',
+    version='0.1.0',
+    package_dir={"": "python"},
+    packages=find_packages("python"),
+    py_modules=[splitext(basename(path))[0] for path in glob('python/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=2.7",
+    install_requires=_requires_from_file('requirements.txt'),
+)
